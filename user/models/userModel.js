04bc073d071a8f61110c2fd053fabe0fd81207e0
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-02-12 22:09:54 +0530

    user-model completed

---
+++ user/models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose=require("mongoose");
+const schema=mongoose.Schema;
+const userdata=new schema(
+    {
+        photo:{
+           type:String,
+        required:true,
+
+        },
+        fristname:{
+            type:String,
+            required:true,
+
+        },
+        lastname:{
+            type:String,
+            required:true,
+
+        },
+        username:{
+            type:String,
+            required:true,
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        fathername:{
+            type:String,
+            required:true,
+        },
+        mobileno:{
+            type:Number,
+            required:true,
+        },
+        caste:{
+            type:String,
+            required:true,
+        },
+        village:{
+            type:String,
+            required:true,
+
+        },
+        mandal:{
+            type:String,
+            required:true,
+        },
+        district:{
+            type:String,
+            required:true,
+        }
+},
+{
+   timestamps:true, 
+}
+);
+module.exports=mongoose.model("userdata",userdata);
