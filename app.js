845e0b5a4d13cafa6b717029006d283430ef1630
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-02-12 19:39:26 +0530

    app.js file completed

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 const express=require("express");
 const mongoose=require("mongoose");
+
+const app=express();
+const cors=require("cors");
+const dotenv=require("dotenv");
+dotenv.config();
+
+
+app.use(express.json());
+app.use(cors());
+
+corsOptions={
+    origin:["http://localhost:6000"],
+};
+PORT=6000;
+
+db_Url="mongodb://localhost:27017/politicianback-end";
+
+mongoose
+.connect(db_Url)
+.then(console.log("db connected"))
+.catch((err) => {
+    console.log("db not connected");
+});
+
+
+app.listen(PORT,() => {
+    console.log("server start on port 6000");
+})
